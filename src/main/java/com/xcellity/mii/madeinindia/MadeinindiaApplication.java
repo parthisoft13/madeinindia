b27c7e630997fda077994abdb21448b9200b4590
parthi <parthisoft13@gmail.com>
2023-07-18 19:11:13 +0530

    made in india code

---
+++ src/main/java/com/xcellity/mii/madeinindia/MadeinindiaApplication.java
@@ -0,0 +1,13 @@
+package com.xcellity.mii.madeinindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadeinindiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadeinindiaApplication.class, args);
+	}
+
+}
